16e65253b8a93f26587fff2575120460bf20ab1c
JonWong2020 <jonathannwong50@aol.com>
2022-10-11 01:30:11 -0700

    added api routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,78 +2,82 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, (req, res) => {
-    Post.findAll({
-            where: {
-                user_id: req.session.user_id
-            },
-            attributes: [
-                'id',
-                'title',
-                'content',
-                'created_at'
-            ],
-            include: [{
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+                where: {
+                    user_id: req.session.user_id
+                },
+                attributes: [
+                    'id',
+                    'title',
+                    'content',
+                    'created_at'
+                ],
+                include: [{
+                        model: Comment,
+                        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                        include: {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    },
+                    {
                         model: User,
                         attributes: ['username']
                     }
-                },
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ]
-        })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
+                ]
+            });
+
+            const posts = postData.map(post => post.get({ plain: true }));
+
             res.render('dashboard', { posts, loggedIn: true });
-        })
-        .catch(err => {
+
+        } catch (err){
             console.log(err);
             res.status(500).json(err);
-        });
+        };
 });
 
-router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findOne({
-            where: {
-                id: req.params.id
-            },
-            attributes: ['id',
-                'title',
-                'content',
-                'created_at'
-            ],
-            include: [{
-                    model: User,
-                    attributes: ['username']
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findOne({
+                where: {
+                    id: req.params.id
                 },
-                {
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
+                attributes: ['id',
+                    'title',
+                    'content',
+                    'created_at'
+                ],
+                include: [{
                         model: User,
                         attributes: ['username']
+                    },
+                    {
+                        model: Comment,
+                        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                        include: {
+                            model: User,
+                            attributes: ['username']
+                        }
                     }
-                }
-            ]
-        })
-        .then(dbPostData => {
-            if (!dbPostData) {
+                ]
+            })
+      
+            if (!postData) {
                 res.status(404).json({ message: 'No post found with this id' });
                 return;
             }
 
-            const post = dbPostData.get({ plain: true });
+            const post = postData.get({ plain: true });
+
             res.render('edit-post', { post, loggedIn: true });
-        })
-        .catch(err => {
+        
+        } catch (err) {
             console.log(err);
             res.status(500).json(err);
-        });
+        };
 });
 
 router.get('/new', (req, res) => {
